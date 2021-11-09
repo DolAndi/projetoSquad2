311653dcfcb6e718cfaadb06aaedea217c3824b2
Kaline Maciel_ <kaline.santos_BOLS@compasso.com.br>
2021-11-09 15:52:58 -0300

    carrinho/contrato att

--- cypress/integration/api_carrinho.spec.js
+++ cypress/integration/api_carrinho.spec.js
@@ -2,21 +2,26 @@
 
 import Factory from '../dynamics/factory'
 
+
 describe("Teste da rota /login para execução posterior da rota /carrinhos", () => {
     it("Deve validar o login com status code 200 e authorization", () => {
         cy.fixture("loginCredentials").then((user) => {
             cy.logar(user.valido).then( res => {
                 expect(res.status).to.equal(200)
                 expect(res.body).to.have.property("authorization")
-                bearer = res.body.authorization
             })
         })
     })
+})
+describe('Testes na rota /carrinhos e validações de contrato', () => {
     it('Deve listar carrinhos cadastrados', () => {
         cy.listarCarrinhos().then(res => {
             expect(res.status).to.be.equal(200)
             expect(res.body).to.have.property('quantidade')
             expect(res.body).to.have.property('carrinhos')
+        cy.validarContrato(res, "get_carrinhos", 200).then(validacao =>{
+            expect(validacao).to.be.equal("Contrato validado!")
+            }) 
         })
     })
 })
